5252b741aa3121fb6a71be041e16dd99806c59e3
Tabitha Buder <tabibuder@gmail.com>
2024-03-06 11:37:10 -0800

    added product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,9 +39,9 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
-    const categoryData = await Category.update(req.body, { where: { id: req.params.id, }});
+    const categoryData = await Category.update(req.body, { where: { id: req.params.id, } });
 
     if (!categoryData) {
       res.status(404).json({ message: 'Could not find Category that matched given id.' });
@@ -53,8 +53,18 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({ where: { id: req.params.id } });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Could not find Category that matched given id.' });
+      return;
+    }
+    return res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
