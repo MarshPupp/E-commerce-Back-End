1cc778b56a147d7ef216c31a88d22a6c54f594fc
Tabitha Buder <tabibuder@gmail.com>
2024-03-05 17:28:21 -0800

    added category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,22 +3,54 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    return res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Could not find Category that matched given id.' });
+      return;
+    }
+    return res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    return res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, { where: { id: req.params.id, }});
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Could not find Category that matched given id.' });
+      return;
+    }
+    return res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
