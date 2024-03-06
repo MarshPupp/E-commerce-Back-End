6d8b7665e31da76e7b273894f7ac8c426f5e3e64
Tabitha Buder <tabibuder@gmail.com>
2024-03-06 15:49:36 -0800

    bug fixes

--- models/Tag.js
+++ models/Tag.js
@@ -8,7 +8,7 @@ Tag.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: false,
+      primaryKey: true,
       allowNull: false,
       autoIncrement: true
     },
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model:Category, attributes: ['category_name'] }, { model: Tag, attributes: ['tag_name'] }]
+      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, attributes: ['tag_name'] }]
     });
     return res.status(200).json(productData);
   } catch (err) {
