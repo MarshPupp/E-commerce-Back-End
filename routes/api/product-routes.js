49c6751860781663d7ada01d0577d8d0f3c23fb4
Tabitha Buder <tabibuder@gmail.com>
2024-03-06 13:09:06 -0800

    added tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,7 +110,17 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({ where: { id: params.id } });
+
+    if (!productData) {
+      res.status(404).json({ message: 'Could not find Product that matched given id.' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
